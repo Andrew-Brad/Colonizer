4705a78dd0ac3b662b4f27b53d6e4b418a26c254
Andrew <zep519@yahoo.com>
2019-04-03 09:35:38 -0500

    Create Powershell_Profile.ps1

---
+++ Windows Client/Powershell_Profile.ps1
@@ -0,0 +1,11 @@
+message = "Profile loaded."
+Write-Output message
+
+Set-Alias -Name tf -Value terraform
+
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
