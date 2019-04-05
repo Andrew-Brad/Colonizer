fc8f9b25cb3a2c8e75a174af693baa713cb6a160
Andrew <zep519@yahoo.com>
2019-04-05 12:40:42 -0500

    Update Powershell_Profile.ps1

--- Windows Client/Powershell_Profile.ps1
+++ Windows Client/Powershell_Profile.ps1
@@ -3,7 +3,13 @@ Write-Output message
 
 Set-Alias -Name tf -Value terraform
 
+Function binobj
+{
+ Get-ChildItem .\ -include bin,obj -Recurse | foreach ($_) { remove-item $_.fullname -Force -Recurse }
+}
 
+# Still not sure how shared Cmder installs pass through portable profiles
+# https://github.com/cmderdev/cmder/issues/505
 # Chocolatey profile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
