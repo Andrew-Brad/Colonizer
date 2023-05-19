7b0b61ebbbb3fc4145e313e6ef0988d8a2e74bb0
Andrew Brad <zep519@yahoo.com>
2023-05-18 20:37:58 -0500

    kubuntu setup script

---
+++ os/Kubuntu Client/initial_setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+sudo mkdir ~/Desktop/Git &&
+cd ~/Desktop/Git &&
+git clone https://github.com/Andrew-Brad/Colonizer.git;
+
+# Apps and software
+sudo snap install --classic code;
+sudo snap install termius-app signal-desktop;
