e270b375c87dd5e5a51d6eab8b1e261c1cc1dec3
Andrew Brad <zep519@yahoo.com>
2023-05-21 22:20:18 -0500

    add 1password to snap installs, rm mouse profile

--- os/Kubuntu Client/initial_setup.sh
+++ os/Kubuntu Client/initial_setup.sh
@@ -6,4 +6,4 @@ git clone https://github.com/Andrew-Brad/Colonizer.git;
 
 # Apps and software
 sudo snap install --classic code;
-sudo snap install termius-app signal-desktop;
+sudo snap install termius-app signal-desktop 1password;
