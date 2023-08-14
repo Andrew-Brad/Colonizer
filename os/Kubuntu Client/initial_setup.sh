c0d943bf8452eedac493ab5b2c121191f7875bc0
Andrew Brad <zep519@yahoo.com>
2023-08-13 21:56:52 -0500

    echo statement in setup.sh

--- os/Kubuntu Client/initial_setup.sh
+++ os/Kubuntu Client/initial_setup.sh
@@ -7,3 +7,6 @@ git clone https://github.com/Andrew-Brad/Colonizer.git;
 # Apps and software
 sudo snap install --classic code;
 sudo snap install termius-app signal-desktop 1password;
+
+# Also copy git and bash files as appropriate
+echo "Script is complete installing snaps. Execute 'reload' in your shell, and copy your .git files and bash files as needed!";
