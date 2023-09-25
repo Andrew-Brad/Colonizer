640e3d9c99906d3f0736f63e0a3ca3d64ef7d835
Andrew <zep519@yahoo.com>
2023-09-25 15:06:05 -0500

    nixos subdir and code extensions for remote-ssh

---
+++ os/linux/nixos/flake.nix
@@ -0,0 +1,13 @@
+# default putput from nix flake init
+
+{
+  description = "A very basic flake";
+
+  outputs = { self, nixpkgs }: {
+
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+  };
+}
