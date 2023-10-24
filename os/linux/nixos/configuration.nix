c576c41421c152f1bc67d4274dcebe8d304de4d4
Andrew Brad <me@andrewbrad.me>
2023-10-23 19:13:35 -0500

    use global pkgs

--- os/linux/nixos/configuration.nix
+++ os/linux/nixos/configuration.nix
@@ -71,7 +71,7 @@
         pkgs.httpie
         ];
       programs.bash.enable = true;
-
+      home-manager.useGlobalPkgs = true;
       # The state version is required and should stay at the version you
       # originally installed.
       home.stateVersion = "23.05";
