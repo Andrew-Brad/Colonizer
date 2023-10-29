6ff75c8c2301ff3d40ea849302aa3db42479425e
Andrew Brad <me@andrewbrad.me>
2023-10-29 14:56:59 -0500

    gen'ed config.nix file, testing if it works

---
+++ os/linux/nixos/ai-configuration.nix
@@ -0,0 +1,48 @@
+{ config, pkgs, ... }:
+
+let
+  myRepo = import ./path/to/repo { inherit pkgs; };
+in
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      '<home-manager/nixos>' # Enable the Home Manager module for NixOS
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.Bobo = {
+    isNormalUser = true;
+    home = "/home/Bobo";
+    description = "Bobo's user";
+    extraGroups = [ "wheel" "docker" ]; # Enable ‘sudo’ for Bobo and add to docker group
+    openssh.authorizedKeys.keys = [ "your_ssh_public_key" ];
+    packages = with pkgs; [ broot htop ]; # Install broot and htop for Bobo
+  };
+
+  services.openssh.enable = true;
+
+  # Enable Docker
+  virtualisation.docker.enable = true;
+  virtualisation.docker.autoPrune.enable = true;
+
+  # Home Manager configuration for the Bobo user
+  home-manager.users.Bobo = { pkgs, ... }: {
+    home.packages = with pkgs; [ broot htop ]; # Install broot and htop for Bobo
+
+    programs.bash.aliases = {
+      ll = "ls -l";
+      la = "ls -A";
+      lla = "ls -la";
+      g = "git";
+    };
+  };
+
+  system.stateVersion = "23.05"; # Did you read the comment?
+}
