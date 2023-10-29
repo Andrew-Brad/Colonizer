9de66d3dadff0d8f850f6e36796cfee5521eddb5
Andrew Brad <me@andrewbrad.me>
2023-10-29 18:01:34 -0500

    bootload error

--- os/linux/nixos/ai-configuration.nix
+++ os/linux/nixos/ai-configuration.nix
@@ -23,8 +23,6 @@ in
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.useOSProber = true;
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos"; # Define your hostname.
 
