b3ed9989b18dd0ce394ca55b21846c41ebb9b1d6
Andrew Brad <me@andrewbrad.me>
2023-10-29 15:09:09 -0500

    try remote repo import

--- os/linux/nixos/ai-configuration.nix
+++ os/linux/nixos/ai-configuration.nix
@@ -1,7 +1,14 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 let
-  myRepo = import ./path/to/repo { inherit pkgs; };
+  myRepo = pkgs.fetchFromGitHub {
+    owner = "Andrew-Brad";
+    repo = "Colonizer";
+    rev = "master";
+    sha256 = "9FFB7B62056BA90185881F2565177463E1E22A94FC6AE4EDF932EB682A8A7F57"; # Replace with the actual SHA-256 hash of the repository
+  };
+
+  myFile = builtins.readFile "${myRepo}/README.md"; # Replace with the actual path to the file in the repository
 in
 {
   imports =
@@ -35,14 +42,14 @@ in
   # Home Manager configuration for the Bobo user
   home-manager.users.Bobo = { pkgs, ... }: {
     home.packages = with pkgs; [ broot htop ]; # Install broot and htop for Bobo
-
-    programs.bash.aliases = {
-      ll = "ls -l";
-      la = "ls -A";
-      lla = "ls -la";
-      g = "git";
-    };
   };
 
   system.stateVersion = "23.05"; # Did you read the comment?
+
+  environment.systemPackages = with pkgs; [
+    (pkgs.writeTextFile {
+      name = "myFile.md";
+      text = myFile;
+    })
+  ];
 }
