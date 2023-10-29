ec7e7f86f73ea78d5c2011bd67ffb625028980e0
Andrew Brad <me@andrewbrad.me>
2023-10-29 17:54:30 -0500

    try more boot vars

--- os/linux/nixos/ai-configuration.nix
+++ os/linux/nixos/ai-configuration.nix
@@ -20,6 +20,9 @@ in
     ];
 
   # Use the systemd-boot EFI boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.useOSProber = true;
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
